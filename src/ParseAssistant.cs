f65a9d3f249a97e4f71eac399457ba38420f0972
Andre Miras <andre.miras@oridao.com>
2015-12-10 16:29:33 +0100

    fixes ParseAssistant.cs namespace

--- src/ParseAssistant.cs
+++ src/ParseAssistant.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
-namespace SensorsMonitoringCF.Database
+namespace SQLite
 {
     /// <summary>
     /// Contains methods to assist with parsing one value into another.
